cb236537875682294ec78ff641275d9aa570159a
RahulAid <raccess97@gmail.com>
2022-10-14 19:44:46 +0530

    Program to check whether Employee is Present or Absent

---
+++ UC1EmployeePresence/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace RANDOM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Fulltime = 1;
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == Fulltime)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else 
+            {
+                Console.WriteLine("Employee is Absent");
+                   
+            }
+        }
+    }
+}
