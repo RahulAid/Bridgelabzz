fad5a8171184ae8ab328bb90a5f7d7ff1561b2c1
RahulAid <raccess97@gmail.com>
2022-10-14 19:48:29 +0530

    Program to calculate Employee Wage

---
+++ UC2EmployeeWage/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == time)
+            {
+                empHrs = 8;
+
+            }
+            else
+            {
+                empHrs = 0;
+
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
+            
+        }
+    }
+}
