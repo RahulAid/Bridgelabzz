62e49144cad75471fc373e7384e137674769e104
RahulAid <raccess97@gmail.com>
2022-10-15 07:24:55 +0530

    Program to calculate Employee Wage for a Month

---
+++ UC5Wforamonth/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace UC5Wforamonth
+{
+    internal class Program
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NO_OF_WORKING_DAYS = 22;
+        static void Main(string[] args)
+        {
+            //Variables
+
+            int empHrs = 0;
+            int empWage = 0;
+            int totalEmpWage = 0;
+
+            //computation
+
+            //for (int day = 0; day < NO_OF_WORKING_DAYS; day++)
+            //{
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage = empWage * NO_OF_WORKING_DAYS;
+                Console.WriteLine("Total working hours : " + empHrs * NO_OF_WORKING_DAYS +);
+            //}
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+        }
+
+    }
+}
