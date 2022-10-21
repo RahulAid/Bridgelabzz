55de0ddb02eab07438b0900b10baa09664eb63bf
RahulAid <raccess97@gmail.com>
2022-10-21 07:57:17 +0530

    Program to calculate wage for multiple companies

---
+++ UC10wageformultiplecomps/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace UC10Ewageformultiplecomps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("Microsoft", 100, 20, 100);
+            empWageBuilder.addCompanyEmpWage("Google", 200, 30, 150);
+            empWageBuilder.computeEmpWage();
+            Console.ReadLine();
+        }
+    }
+}
